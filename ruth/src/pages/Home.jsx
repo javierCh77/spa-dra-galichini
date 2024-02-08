25c06e8492b8ea6623d63b8580ec1f82db8a416a
javierCh77 <javierchavarria21@gmail.com>
2024-02-08 11:57:57 -0300

    update

--- ruth/src/pages/Home.jsx
+++ ruth/src/pages/Home.jsx
@@ -12,6 +12,11 @@ const Home = () => {
             <img src={logor} alt='logo dra. Ruth galichini ' className='w-52 md:w-96'></img>
         </div>
        </div>
+       <div className='container-fluid justify-center text-center align-middle flex p-5 md:p-10  rounded'>
+         <div className='bg-gray-500 container  p-6 rounded-lg '>
+               <p className='text-white text-2xl md:text-4xl'>Especialidad</p>
+          </div>
+       </div>
        
        
     </>
